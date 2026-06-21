8aba27c1f64893d9a821706a259f22bc80890567
psi-utas <myhr.support@utas.edu.au>
2026-06-21 21:31:26 +1000

    Update app.py

--- app.py
+++ app.py
@@ -59,42 +59,7 @@ generation_config = {
     "top_k": 1,
     "response_mime_type": "application/json"
 }
-"""
-with st.sidebar:
-    st.subheader("Gemini API")
 
-    st.link_button(
-        "Get Gemini API Key",
-        "https://aistudio.google.com/u/1/api-keys",
-        use_container_width=True
-    )
-
-    gemini_api_key = st.text_input(
-        "Gemini API Key",
-        type="password"
-    )
-
-    st.caption(
-        "Your API key is used only during this session."
-    )
-
-if not gemini_api_key:
-    st.info("Enter your Gemini API Key to continue.")
-    st.stop() ##
-
-# GEMINI CONFIG
-genai.configure(api_key=gemini_api_key)
-
-MODEL_NAME = "gemini-3.1-flash-lite"
-model = genai.GenerativeModel(MODEL_NAME)
-
-generation_config = {
-    "temperature": 0,
-    "top_p": 0.1,
-    "top_k": 1,
-    "response_mime_type": "application/json" 
-}
-"""
 # PROMPTS
 SINGLE_PROMPT = """
 You are a payroll classification engine.
